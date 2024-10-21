00c4b4d7b9ab636e3aaa7ebb35aacc5923f277e9
Spectrie <victor.campbell10@outlook.com>
2024-10-21 11:55:01 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Teste from './Teste'
+import EstiloGlobal from './styles'
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+  )
+}
+
+export default App
