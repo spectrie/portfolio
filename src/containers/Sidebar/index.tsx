2f003c1a5dcfdc875c7031f18f67913737147ffb
Spectrie <victor.campbell10@outlook.com>
2024-10-22 15:57:29 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/spectrie.png" alt="" />
-    <Titulo fontSize={20}>Marcello Campbell</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Marcello Campbell</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        spectrie
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
--- src/styles.ts
+++ src/styles.ts
@@ -14,6 +14,8 @@ body{
   @media (max-width: 768px){
   padding-top: 16px
   }
+
+  padding-bottom: 80px
 }
 `
 export default EstiloGlobal
@@ -29,9 +31,5 @@ export const Container = styled.div`
   @media (max-width: 768px) {
     max-width: 80%;
     display: block;
-  }
-
-  img {
-    max-width: 100%;
   }
 `
